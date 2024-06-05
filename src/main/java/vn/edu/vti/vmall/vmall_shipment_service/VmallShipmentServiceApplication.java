da4797b317803f93cfe44bb9aff4194e6babdd72
tunk <nguyenkhactu1996@gmail.com>
2024-06-05 10:42:44 +0700

    Initial commit

---
+++ src/main/java/vn/edu/vti/vmall/vmall_shipment_service/VmallShipmentServiceApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.vti.vmall.vmall_shipment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmallShipmentServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VmallShipmentServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/vn/edu/vti/vmall/vmall_shipment_service/VmallShipmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.vti.vmall.vmall_shipment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmallShipmentServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
